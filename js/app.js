1cf2fe09f5aaafa29fa106e81e027ac4cc80de97
Alex Chao <chaoalex93@gmail.com>
2023-05-08 21:07:56 +0000

    store object created, linked section id from app.js to index.html

--- js/app.js
+++ js/app.js
@@ -1,2 +1,44 @@
 'use strict';
 
+let cookieSection = document.getElementById('cookie-profiles');
+
+console.dir(cookieSection);
+
+let hours = ['6am', '7am', '8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm', '7pm'];
+
+
+let seattle = {
+  name: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookieBought: 6.3,
+};
+
+let tokyo = {
+  name: 'Tokyo',
+  minCust: 3,
+  maxCust: 24,
+  avgCookieBought: 1.2,
+};
+
+let dubai = {
+  name: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  avgCookieBought: 3.7,
+};
+
+let paris = {
+  name: 'Paris',
+  minCust: 20,
+  maxCust: 38,
+  avgCookieBought: 2.3,
+};
+
+let lima = {
+  name: 'Lima',
+  minCust: 2,
+  maxCust: 16,
+  avgCookieBought: 4.6,
+};
+
